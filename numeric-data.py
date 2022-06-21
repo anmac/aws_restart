62b598cbf9387cae3c11adaf4f45f92bde29fee1
Miguel Maza <69657880+anmac@users.noreply.github.com>
2022-06-21 17:28:20 -0500

    First commit

---
+++ numeric-data.py
@@ -0,0 +1,16 @@
+myValue = 1
+myFloat=3.14
+myComplex=5j
+myBoolean= True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(myFloat)
+print(type(myFloat))
+print(str(myFloat) + " is of the data type " + str(type(myFloat)))
+print(myComplex)
+print(type(myComplex))
+print(str(myComplex) + " is of the data type " + str(type(myComplex)))
+print(myBoolean)
+print(type(myBoolean))
+print(str(myBoolean) + " is of the data type " + str(type(myBoolean)))
